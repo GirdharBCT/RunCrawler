d0acc5efab93e830e4fba28c10d4efaf25bcf1b3
GirdharBCT <Girdhar.Sahu@BlueconchTech.com>
2022-05-18 12:17:57 +0530

    Check/poll SQS messages for s3 events

--- Function.cs
+++ Function.cs
@@ -11,6 +11,12 @@ using System.Text.Json;
 using System.Threading;
 using Newtonsoft.Json;
 using System.IO;
+using Amazon.SQS.Model;
+using Amazon.SQS;
+using Newtonsoft.Json.Linq;
+using Amazon.Lambda.SQSEvents;
+using static Amazon.Lambda.SQSEvents.SQSEvent;
+using Amazon.S3.Util;
 
 // Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
@@ -20,6 +26,7 @@ namespace RunCrawler
     public class Function
     {
         IAmazonGlue GlueClient { get; set; }
+        IAmazonSQS AmazonSQSClient { get; }
 
         /// <summary>
         /// Default constructor. This constructor is used by Lambda to construct the instance. When invoked in a Lambda environment
@@ -29,15 +36,17 @@ namespace RunCrawler
         public Function()
         {
             GlueClient = new AmazonGlueClient();
+            AmazonSQSClient = new AmazonSQSClient();
         }
 
         /// <summary>
         /// Constructs an instance with a preconfigured S3 client. This can be used for testing the outside of the Lambda environment.
         /// </summary>
         /// <param name="glueClient"></param>
-        public Function(IAmazonGlue glueClient)
+        public Function(IAmazonGlue glueClient, IAmazonSQS amazonSQS)
         {
             this.GlueClient = glueClient;
+            this.AmazonSQSClient = amazonSQS;
         }
         
         /// <summary>
@@ -51,62 +60,78 @@ namespace RunCrawler
             public int httpStatusCode { get; set; }
             public string body { get; set; }
         }
-        
-        public async Task<HttpResponse> FunctionHandler(ILambdaContext context)
+
+        private async Task<ReceiveMessageResponse> GetMessage()
         {
-            var watch = System.Diagnostics.Stopwatch.StartNew();
+            return await AmazonSQSClient.ReceiveMessageAsync(new ReceiveMessageRequest
+            {
+                QueueUrl = "https://sqs.ap-south-1.amazonaws.com/436728321574/DVTFileUpload"
+            });
+        }
 
-            int MAX_RETRY = 20;
-            var request = new StartCrawlerRequest
+        public async Task<HttpResponse> FunctionHandler(ILambdaContext context,Message message)
+        {
+           
+
+
+            var recivedMsgResponse = await GetMessage();
+            var temp = recivedMsgResponse.Messages[0].Body;
+            context.Logger.Log($"sqsEventMessage ->\n{temp}");
+            var s3Event = JsonConvert.DeserializeObject<S3EventNotification>(temp);
+            var s3Object = s3Event.Records[0].S3.Object;
+            context.Logger.Log($"{s3Object}");
+            //s3Object = null;
+            if (s3Object != null)
             {
-                Name = "Demo-DVT-Crawler"
-            };
-            var response = await GlueClient.StartCrawlerAsync(request);
-            var retry_count = 1;
-            var crawler_status = new GetCrawlerResponse();
-            while (retry_count < MAX_RETRY)
-            {
-                //Thread.Sleep(30);
-                await Task.Delay(5000);
-                LambdaLogger.Log("MY Current Counter"+ retry_count.ToString());
-                LambdaLogger.Log(crawler_status?.Crawler?.State?.Value);
-                crawler_status = await GlueClient.GetCrawlerAsync(new GetCrawlerRequest { Name = "Demo-DVT-Crawler" });
-                LambdaLogger.Log(crawler_status?.Crawler?.State?.Value);
-                //LambdaLogger.Log(crawler_status.ToString());
-                var crawler_run_status = crawler_status.Crawler.State.Value;
-                if (crawler_run_status == "READY")
+                var watch = System.Diagnostics.Stopwatch.StartNew();
+
+                int MAX_RETRY = 20;
+                var request = new StartCrawlerRequest
                 {
-                    break;
+                    Name = "Demo-DVT-Crawler"
+                };
+                var response = await GlueClient.StartCrawlerAsync(request);
+                var retry_count = 1;
+                var crawler_status = new GetCrawlerResponse();
+                while (retry_count < MAX_RETRY)
+                {
+                    //Thread.Sleep(30);
+                    await Task.Delay(5000);
+                    //LambdaLogger.Log("MY Current Counter" + retry_count.ToString());
+                    //LambdaLogger.Log(crawler_status?.Crawler?.State?.Value);
+                    crawler_status = await GlueClient.GetCrawlerAsync(new GetCrawlerRequest { Name = "Demo-DVT-Crawler" });
+                    //LambdaLogger.Log(crawler_status?.Crawler?.State?.Value);
+                    //LambdaLogger.Log(crawler_status.ToString());
+                    var crawler_run_status = crawler_status.Crawler.State.Value;
+                    if (crawler_run_status == "READY")
+                    {
+                        break;
+                    }
+                    retry_count += 1;
                 }
-                retry_count += 1;
+                context.Logger.Log($"Crawler_status.tostring() -> {crawler_status.ToString()}");
+
+                // the code that you want to measure comes here
+                watch.Stop();
+                var elapsedMs = watch.ElapsedMilliseconds;
+                context.Logger.Log($"\nTime Elapsed {elapsedMs}");
+                return new HttpResponse
+                {
+                    httpStatusCode = 200,
+                    body = "Crawler Completes."
+                };
+                //return new HttpResponse
+                //{
+                //    httpStatusCode = 200,
+                //    body = recivedMsgResponse.ToString()
+                //};
             }
-            context.Logger.Log($"Crawler_status.tostring() -> {crawler_status.ToString()}");
-            
-            // the code that you want to measure comes here
-            watch.Stop();
-            var elapsedMs = watch.ElapsedMilliseconds;
-            context.Logger.Log($"\nTime Elapsed {elapsedMs}");
-            return new HttpResponse 
-            { 
-                httpStatusCode = 200,
-                body="Crawler Completes."
+
+            return new HttpResponse
+            {
+                httpStatusCode = 400,
+                body = "No new s3 object found"
             };
-
-
-            //var request = new GetCrawlerRequest 
-            //{ 
-            //    Name = "Demo-DVT-Crawler" 
-            //};
-            //var response = await GlueClient.GetCrawlerAsync(request);
-            //LambdaLogger.Log($"response-->\n{response}");
-            //LambdaLogger.Log($"response.tostring-->\n{response.ToString()}");
-            //var temp = response.Crawler.State;
-            //LambdaLogger.Log(temp);
-            //LambdaLogger.Log($"response.type-->\n{typeof(response)}");
-
-            //var temp= JsonConvert.DeserializeObject(response);
-            //return JsonSerializer.Deserialize<String>(response)!;
-
         }
     }
 }
